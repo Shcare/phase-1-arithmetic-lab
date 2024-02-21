fed3231d586447e439873b1c58645a6475f423d5
Shcare <shcareee@gmail.com>
2024-02-21 23:50:56 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1=2
+let num2=31
+const multiply =num1*num2
+
+const random = Math.ceil(Math.random());
+let num3=9
+let num4=5
+const mod = num3%num4;
+
+const max = Math.max(1, 20, 3, 4, 5);
